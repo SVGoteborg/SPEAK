fe795dc7923fa98386c81cd17952a039b6828d45
PetNil <peter.work@live.se>
2021-02-11 10:51:59 +0100

    Update more paraameters for JSON file

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -10,6 +10,7 @@ using MimeKit;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using MailKit.Net.Pop3;
 
 class Program
 {
@@ -19,7 +20,7 @@ class Program
 
     //Kommer inte att behövas -----
     private static string FileFromName2 = "MSG00001.wav";
-    private static string FileToRead = FileFromPath + "\\" + FileFromName2;
+    private static string FileToSave = FileFromPath + "\\" + FileFromName2;
     private const string FileToPath = "C:\\Users\\admin\\Desktop\\AppSpeak\\Result";
     private const string FileToName = "WriteText2.txt";
     private const string FileToWrite = FileToPath + "\\" + FileToName;
@@ -30,8 +31,11 @@ class Program
     private static string fromEmail = null;
     //private static string fromName = "Avvikelse";
     //private static string fromEmail = "avvikelse@testboka.net";
-    private static string Client = "smtp.simply.com";
-    private static int port = 587;
+    private static string fromClient = "mail.simply.com";
+    private static int fromPort = 110;
+    private static int fromImapPort = 143;
+    private static string toClient = "smtp.simply.com";
+    private static int toPort = 587;
     private static string passw = "Elektronik!100";
     private static string toName = "Avvikelse2";
     private static string toEmail = "avvikelse2@testboka.net";
@@ -41,25 +45,72 @@ class Program
 
     async static Task FromFile(SpeechConfig speechConfig)
     {
-        string[] fileArray = Directory.GetFiles(FileFromPath, "*.wav");
-        FileFromName = fileArray[0];
-        using var audioConfig = AudioConfig.FromWavFileInput(FileFromName);
-        //using var audioConfig = AudioConfig.FromWavFileInput("PathToFile.wav");
-        using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+        var fileToSave = FileToSave;
 
-        var result = await recognizer.RecognizeOnceAsync();
-        Console.WriteLine($"RECOGNIZED: Text={result.Text}");
-        // Skall tas bort
-        SaveText(result.Text);
+        using (var client = new Pop3Client())
+        {
+            client.Connect(fromClient, fromPort, false);
 
-        TranscribedMessage = result.Text;
+            client.Authenticate(fromEmail, passw);
+
+            for (int i = 0; i < client.Count; i++)
+            {
+                var message = client.GetMessage(i);
+
+                foreach (var attachment in message.Attachments)
+                {
+                    if (attachment is MessagePart)
+                    {
+                        var fileName = attachment.ContentDisposition?.FileName;
+                        var rfc822 = (MessagePart)attachment;
+
+                        if (string.IsNullOrEmpty(fileName))
+                            fileName = "attached-message.eml";
+
+                        using (var stream = File.Create(fileName))
+                            rfc822.Message.WriteTo(stream);
+                    }
+                    else
+                    {
+                        var part = (MimePart)attachment;
+                        var fileName = part.FileName;
+
+                        using (var stream = File.Create(fileName))
+                        {
+                            part.Content.DecodeTo(stream);
+                        }
+
+                        using (var stream = File.Create(fileToSave))
+                        {
+                            part.Content.DecodeTo(stream);
+                        }
+                    }
+                }
+                Console.WriteLine("Subject: {0}", message.Subject);
+
+            }
+            client.Disconnect(true);
+        }
+
+        //string[] fileArray = Directory.GetFiles(FileFromPath, "*.wav");
+        //FileFromName = fileArray[0];
+        //using var audioConfig = AudioConfig.FromWavFileInput(FileFromName);
+        ////using var audioConfig = AudioConfig.FromWavFileInput("PathToFile.wav");
+        //using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        //var result = await recognizer.RecognizeOnceAsync();
+        //Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        //// Skall tas bort
+        //SaveText(result.Text);
+
+        //TranscribedMessage = result.Text;
 
     }
     // Skall tas bort
-    static void SaveText(string text)
-    {
-        File.WriteAllText(FileToWrite, text);
-    }
+    //static void SaveText(string text)
+    //{
+    //    File.WriteAllText(FileToWrite, text);
+    //}
 
     static void readJson()
     {
@@ -76,8 +127,11 @@ class Program
         FileFromPath = (string)o1["FileFromPath"];
         fromName = (string)o1["fromName"];
         fromEmail = (string)o1["fromEmail"];
-        Client = (string)o1["Client"];
-        port = (int)o1["port"];
+        fromClient = (string)o1["fromClient"];
+        fromPort = (int)o1["fromPort"];
+        fromImapPort = (int)o1["fromImapPort"];
+        toClient = (string)o1["toClient"];
+        toPort = (int)o1["toPort"];
         passw = (string)o1["passw"];
         toName = (string)o1["toName"];
         AzureSubscription = (string)o1["AzureSubscription"];
@@ -94,32 +148,32 @@ async static Task Main(string[] args)
         await FromFile(speechConfig);
 
         // Send email
-        var message = new MimeMessage();
-        message.From.Add(new MailboxAddress(fromName, fromEmail));
-        message.To.Add(new MailboxAddress(toName, toEmail));
-        message.Subject = "Transkriberad text";
+        //var message = new MimeMessage();
+        //message.From.Add(new MailboxAddress(fromName, fromEmail));
+        //message.To.Add(new MailboxAddress(toName, toEmail));
+        //message.Subject = "Transkriberad text";
 
-        message.Body = new TextPart("plain")
-        {
-            Text = TranscribedMessage
-        };
+        //message.Body = new TextPart("plain")
+        //{
+        //    Text = TranscribedMessage
+        //};
 
-        using (var client = new SmtpClient())
-        {
-            client.Connect(Client, port, false);
+        //using (var client = new SmtpClient())
+        //{
+        //    client.Connect(toClient, toPort, false);
 
-            // Note: only needed if the SMTP server requires authentication
-            client.Authenticate(fromEmail, passw);
+        //    // Note: only needed if the SMTP server requires authentication
+        //    client.Authenticate(fromEmail, passw);
 
-            client.Send(message);
-            client.Disconnect(true);
-        }
-        DirectoryInfo di = new DirectoryInfo(FileFromPath);
+        //    client.Send(message);
+        //    client.Disconnect(true);
+        //}
+        //DirectoryInfo di = new DirectoryInfo(FileFromPath);
 
-        foreach (FileInfo file in di.GetFiles())
-        {
-            file.Delete();
-        }
+        //foreach (FileInfo file in di.GetFiles())
+        //{
+        //    file.Delete();
+        //}
     }
 
     
